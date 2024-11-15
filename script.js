639fee7171205884a7dc4136e46284ddd9a60ca0
baehl <baehl44@yahoo.fr>
2024-11-15 09:43:57 +0100

    first commit avec la fonction getLatinCharacterList

---
+++ script.js
@@ -0,0 +1,7 @@
+//fonction va prendre en paramètre du texte et retourner un tableau contenant chaque caractère
+
+function getLatinCharacterList(Mytexte){
+    let myTexteArray= Mytexte.split('')
+    console.log(myTexteArray)
+}
+getLatinCharacterList("Hello,world");
